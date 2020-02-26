28efc448d4c676a077a612c6a76b1b6f4bd5eb36
Kevin Udink <kudink@gmail.com>
2020-02-26 15:54:23 -0700

    updated pacman to use objects and jQuery

--- pacman.js
+++ pacman.js
@@ -1,43 +1,27 @@
-const appContainer = document.getElementById("app");
-const scoreDiv = document.getElementById("score");
-let pacmanPosition = {
-  "x": 1,
-  "y": 1
-}
+const appContainer = $("#app");
+let scoreDiv = $("#score");
+
 const blockWidth = 40;
 let score = 0;
-
-// This code will create the following HTML is the resulting web page:
-//    <div class="pacman"> </div>
-const pacman = document.createElement("img");
-pacman.src = "images/pacman.gif";
-pacman.className = "pacman";
-
-// now we will set the position of the pacman image on the page
-pacman.style.left = (pacmanPosition["x"] * blockWidth) + "px";
-pacman.style.top = (pacmanPosition["y"] * blockWidth) + "px";
-
-// add the pacman to the web page
-appContainer.append(pacman);
 
 // create a new div for the world that Pacman will run around in
 const worldMap = document.createElement("div");
 appContainer.append(worldMap);
 
 let world = [
-  [2,2,2,2,2,2,2,2,2,2],
-  [2,0,1,1,1,1,1,1,1,2],
-  [2,1,1,2,2,2,2,1,1,2],
-  [2,1,1,2,1,1,1,1,1,2],
-  [2,1,1,2,2,3,2,2,1,2],
-  [2,1,1,1,1,1,1,2,1,2],
-  [2,1,1,1,1,2,2,2,1,2],
-  [2,1,1,1,1,1,1,1,1,2],
-  [2,2,2,2,2,2,2,2,2,2]
+  [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2],
+  [2,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+  [2,1,1,2,2,2,2,1,1,1,1,1,1,1,1,1,2],
+  [2,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,2],
+  [2,1,1,2,2,3,2,2,1,1,1,1,1,1,1,1,2],
+  [2,1,1,1,1,1,1,2,1,1,1,1,1,1,1,1,2],
+  [2,1,1,1,1,2,2,2,1,1,1,1,1,1,1,1,2],
+  [2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2],
+  [2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2]
 ];
 
-let worldHeight = world.length;
-let worldWidth = world[0].length;
+var worldHeight = world.length;
+var worldWidth = world[0].length;
 let blockType = {
   0: "empty",
   1: "coin",
@@ -45,11 +29,87 @@ let blockType = {
   3: "cherry"
 }
 
-scoreDiv.style.left = (worldWidth * blockWidth + 40) + "px";
-scoreDiv.style.top = "20px";
-scoreDiv.innerHTML = score;
+const pacmanObj = {
+  "image": "images/pacman.gif",
+  // TODO: A feature would be to make this dynamic - let the user choose this by adding a URL to a gif or select some premade images
+  // TODO: "lives": 3,  This needs to be added to the game object
 
-// using the double array above, create the HTML to draw the world that pacman will go through
+  "position": {
+    "x": 1,
+    "y": 1
+  },
+
+  "createPacman": function() {
+    let pacman = document.createElement("img");
+
+    // This code will create the following HTML is the resulting web page:
+    //    <img src="..." class="pacman"> </div>
+    pacman.src = this.image; 
+    pacman.className = "pacman";
+    pacman.id = "hero";
+
+    pacman["style"]["top"] = `${pacmanObj["position"]["y"] * blockWidth}px`;
+    pacman["style"]["left"] = `${pacmanObj["position"]["x"] * blockWidth}px`;
+
+    return pacman;
+  },
+
+  // This doesn't appear to work and so we can get rid of this
+  //    instead, use jquery to grab the object as we need it
+  // "hero": $("#hero"),
+
+  "move": function(XorY, num, rotate) {
+    this["position"][XorY] += num;
+    // string concatination to replace the "rotate(" + rotate + "deg)"
+    // this.hero.style.transform is the same as the following syntax
+    console.log("transform: " + `rotate(${rotate}deg)`);
+    console.log("top:       " + `${pacmanObj["position"]["y"] * blockWidth}px`);
+    console.log("left:      " + `${pacmanObj["position"]["x"] * blockWidth}px`);
+
+    // using a jquery function called css we can use an object with key value pairs to set css properties
+    //     https://api.jquery.com/css/#css-properties
+    // this["hero"].css({
+    $("#hero").css({
+      "transform": `rotate(${rotate}deg)`,
+      "top": `${pacmanObj["position"]["y"] * blockWidth}px`,
+      "left": `${pacmanObj["position"]["x"] * blockWidth}px`
+    });
+    // this["hero"]["css"]("top", `${pacmanObj["position"]["y"] * blockWidth}px`);
+    // this["hero"]["css"]("left", `${pacmanObj["position"]["x"] * blockWidth}px`);
+
+    // ONLY update the score and the board to have the correct layout as pacman eats the coins
+    let newBlockType = world[pacmanObj["position"]["y"]][pacmanObj["position"]["x"]];
+    if (newBlockType == 1 || newBlockType > 2) {
+      console.log ("new block type: " + newBlockType);
+      world[pacmanObj["position"]["y"]][pacmanObj["position"]["x"]] = 0;
+      if (newBlockType == 1) {
+        score += 10;
+      }
+      else {
+        score += 50;
+      }
+      drawWorld();
+      scoreDiv.html("Score: " + score);
+    }
+
+    console.log(`Hero will move to: ${XorY}:${this.position[XorY] * blockWidth}`);
+  }
+}
+
+// console.log("create pacman: " + pacmanObj.createPacman);
+appContainer.append(pacmanObj["createPacman"]());
+
+scoreDiv.css({
+  "top": "20px",
+  "left": (worldWidth * blockWidth + 40) + "px"
+});
+// you can do this with 2 different commands or use the object as shown above
+// scoreDiv.css("top", "20px");
+// scoreDiv.css("left", (worldWidth * blockWidth + 40) + "px");
+scoreDiv.html(score);
+
+
+// using the double array from above, create the HTML to draw the world that pacman will go through
 function drawWorld() {
   let output = "";
   for (var i = 0 ; i < world.length ; i++) {
@@ -63,80 +123,33 @@ function drawWorld() {
 }
 drawWorld();
 
+
 // When a directional arrow is pressed, adjust pacman and the world accordingly
 function onKeyDown(event) {
-  let xPos = pacmanPosition["x"];
-  let yPos = pacmanPosition["y"];
+  let xPos = pacmanObj["position"]["x"];
+  let yPos = pacmanObj["position"]["y"];
 
   // Keycode 37 = arrow left   a == 65
   if (event["keyCode"] === 37) {
-    pacman.style.transform = "rotate(180deg)";
-
-    if (world[yPos][xPos - 1] != 2) {
-      xPos -=1
-      console.log("going left - new position:" + pacman.style.left);
-    } else {
-      console.log("Cannot go left - position is:" + pacman.style.left);
-    }
+    if (world[yPos][xPos - 1] != 2) pacmanObj["move"]("x", -1, 180);
   }
   
   // Keycode 38 = arrow up     w == 87
   else if (event["keyCode"] === 38) {
-    pacman.style.transform = "rotate(270deg)";
-
-    if (world[yPos - 1][xPos] != 2) {
-      yPos -= 1;
-      console.log("going up - new position:" + pacman.style.top);
-    } else {
-      console.log("Cannot go Up - position is:" + pacman.style.top);
-    }
+    if (world[yPos - 1][xPos] != 2) pacmanObj["move"]("y", -1, 270);
   }
 
   // Keycode 39 = arrow right  d == 68
   else if (event["keyCode"] === 39) {
-    pacman.style.transform = "rotate(0deg)";
-    
-    if (world[yPos][xPos + 1] != 2) {
-      xPos +=1
-      console.log("going right - new position:" + pacman.style.left);
-    } else {
-      console.log("Cannot go right - position is:" + pacman.style.left);
-    }
+    if (world[yPos][xPos + 1] != 2) pacmanObj["move"]("x", 1, 0);
   }
 
   // Keycode 40 = arrow down   s == 83
   else if (event["keyCode"] === 40) {
-    pacman.style.transform = "rotate(90deg)";
-
-    if (world[yPos + 1][xPos] != 2) {
-      yPos += 1;
-      console.log("going down - new position:" + pacman.style.top);
-    } else {
-      console.log("Cannot go Down - position is:" + pacman.style.top);
-    }
+    if (world[yPos + 1][xPos] != 2) pacmanObj["move"]("y", 1, 90);
   }
-
-  pacmanPosition["y"] = yPos;
-  pacman.style.top = (yPos * blockWidth) + "px";
-  pacmanPosition["x"] = xPos;
-  pacman.style.left = (xPos * blockWidth) + "px";
-  console.log("xPos: " + xPos + "   yPos: " + yPos);
-
-  // ONLY update the score and the board to have the correct layout as pacman eats the coins
-  let newBlockType = world[yPos][xPos];
-  if (newBlockType == 1 || newBlockType > 2) {
-    world[yPos][xPos] = 0;
-    if (newBlockType == 1) {
-      score += 10;
-    }
-    else {
-      score += 50;
-    }
-    drawWorld();
-    scoreDiv.innerHTML = "Score: " + score;
-  }
-
 }
 
 // run the function "onKeyDown" when a key is pressed
-document.addEventListener("keydown", onKeyDown);
+// document.addEventListener("keydown", onKeyDown);
+$(document).keydown(onKeyDown);
